a500e452c91f7728028879db770c4ee04a8a69f6
Marcel Gern <marcel@familie-gern.de>
2024-08-07 12:27:58 +0200

    works for rafts too

--- test_top_down.py
+++ test_top_down.py
@@ -5,16 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from swarmrl.components import Colloid
 import open3d as o3d
+import logging 
 
+logging.basicConfig(level=logging.WARNING)
 colloids = []
 for i in range(10):
-    colloids.append(Colloid([10000*np.random.rand(),10000*np.random.rand(),0], [np.random.rand(),np.random.rand(),0], i))
-
+    colloids.append(Colloid([9000*np.random.rand()+500,9000*np.random.rand()+500,0], [np.random.rand(),np.random.rand(),0], i))
+# colloids.append(Colloid([0,0,0], [0,0,0], 10))
     
 rafts = o3d.io.read_triangle_mesh("rafts.stl")
-obs = TopDownImage(np.array([10000.0, 10000.0]), particle_type=0,custom_mesh=rafts,is_2D=True)
+# rafts=o3d.geometry.TriangleMesh.create_sphere(radius=100) 
+obs = TopDownImage(np.array([10000.0, 10000.0, 0.1]), particle_type=0,custom_mesh=rafts,is_2D=True)
 m=obs.compute_observable(colloids=colloids)
-m=obs.compute_observable(colloids=colloids)
-print(np.shape(m))
+#
+# print(np.shape(m))
+# plt.imshow(m, cmap='gray')
 plt.imshow(m)
-plt.show()
+plt.savefig("test_top_down.png")
+# plt.show()
