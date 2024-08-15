163b957573d06b0468073eada64c1e359b8341b3
Marcel Gern <marcel@familie-gern.de>
2024-08-15 10:15:43 +0200

    refactored methods and added docstring

--- swarmrl/observables/top_down_image.py
+++ swarmrl/observables/top_down_image.py
@@ -8,15 +8,18 @@ from swarmrl.observables.observable import Observable
 from swarmrl.components.colloid import Colloid
 
 class TopDownImage(Observable, ABC):
+    """
+    The image produced by a top-down view of the scene. Produces an grayscale image similar to a camera image.
+    """
     def __init__(self, box_length: np.ndarray, particle_type: int = 0, custom_mesh=None, is_2D=False):
         """
-        Initializes a TopDownImage object.
+        Initializes the TopDownImage object.
 
         Args:
-            box_length (np.ndarray): The length of the simulation box.
-            particle_type (int, optional): The type of particles to include in the image. Defaults to 0.
-            custom_mesh (optional): A custom mesh to use for the particles.
-            is_2D (bool, optional): Flag indicating whether the simulation is 2D or 3D. Defaults to False.
+            box_length (np.ndarray): The length of the box.
+            particle_type (int, optional): The type of particle. Defaults to 0.
+            custom_mesh (None, optional): Custom mesh. Defaults to None.
+            is_2D (bool, optional): Flag indicating if the image is 2D. Defaults to False.
         """
         self.logger = logging.getLogger(__name__)
         super().__init__(particle_type=particle_type)
@@ -24,34 +27,67 @@ class TopDownImage(Observable, ABC):
         self.particle_type = particle_type
         self.custom_mesh = custom_mesh
         self.is_2D = is_2D
-        self.renderer = o3d.visualization.rendering.OffscreenRenderer(640, 640)
+        self.renderer = o3d.visualization.rendering.OffscreenRenderer(1280, 1280)
         self.renderer.scene.set_background([0, 0, 0, 1])  # Set background to black
         self.logger.debug("TopDownImage initialized")
 
-    def create_bounding_box(self):
+    def create_bounding_box(self) -> None:
         """
-        Creates a bounding box mesh for the simulation.
+        Creates a bounding box in the scene.
+
+        Returns:
+            None
         """
-        # Create a box with the dimensions specified by box_length
         bounding_box = o3d.geometry.TriangleMesh.create_box(width=self.box_length[0], height=self.box_length[1], depth=self.box_length[2])
         bounding_box.paint_uniform_color([1, 1, 1])  # Set bounding box color to white
         self.renderer.scene.add_geometry("bounding_box", bounding_box, o3d.visualization.rendering.MaterialRecord())
         self.logger.debug("Bounding box created")
 
-    def rgb2gray(self, rgb):
-        return np.dot(np.array(rgb[..., :3]), np.array([0.2989, 0.5870, 0.1140]))
-
-    def compute_observable(self, colloids: List[Colloid]):
+    def rgb2gray(self, rgb: np.ndarray) -> np.ndarray:
         """
-        Computes the top-down image based on the given colloids.
+        Convert an RGB image to grayscale.
 
-        Args:
-            colloids (List[Colloid]): A list of Colloid objects representing the particles in the simulation.
+        Parameters:
+            rgb (np.ndarray): The input RGB image.
 
         Returns:
-            np.ndarray: The computed top-down image as a numpy array.
+            np.ndarray: The grayscale image.
+
         """
-        self.create_bounding_box()
+        return np.dot(np.array(rgb[..., :3]), np.array([0.2989, 0.5870, 0.1140]))
+
+
+    def positionCameraAboveCenter(self) -> None:
+        """
+        Positions the camera above the center of the scene. Works only for Gaurav's microbots.
+
+        This method calculates the center of the scene's bounding box and positions the camera
+        above it. The camera is set up with a 60-degree field of view, and the up direction is
+        set to [0, 1, 0].
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        bbox = self.renderer.scene.bounding_box
+        center = bbox.get_center()
+        eye = center + np.array([0, 0, 10000])  # Position the camera above the center
+        up = np.array([0, 1, 0])  # Up direction
+        self.renderer.setup_camera(60, center, eye, up)
+        self.logger.debug(f"Camera setup with center: {center}, eye: {eye}, up: {up}")
+
+    def add_colloids_to_image(self, colloids: List[Colloid]) -> None:
+        """
+        Adds colloids to the image.
+
+        Args:
+            colloids (List[Colloid]): A list of colloids to be added to the image.
+
+        Returns:
+            None
+        """
         for colloid in colloids:
             if colloid.type == self.particle_type:
                 if self.custom_mesh is not None:
@@ -68,21 +104,25 @@ class TopDownImage(Observable, ABC):
                 particle.paint_uniform_color([0.5, 0.5, 0.5])
                 self.renderer.scene.add_geometry(f"particle_{colloid.id}", particle, o3d.visualization.rendering.MaterialRecord())
         self.logger.debug("Colloids added to the image")
-        
-        # Automatically adjust the camera to include all geometries
-        bbox = self.renderer.scene.bounding_box
-        center = bbox.get_center()
-        extent = bbox.get_extent()
-        eye = center + np.array([0, 0, extent[2] * 5])  # Position the camera above the center
-        up = np.array([0, 1, 0])  # Up direction
-        self.renderer.setup_camera(60, center, eye, up)
-        self.logger.debug(f"Camera setup with center: {center}, eye: {eye}, up: {up}")
+
+
+    def compute_observable(self, colloids: List[Colloid]):
+        """
+        Computes the top-down image observable based on the given colloids.
+
+        Args:
+            colloids (List[Colloid]): A list of colloids representing the objects in the scene.
+
+        Returns:
+            np.ndarray: The computed top-down image as a numpy array.
+        """
+        self.create_bounding_box()
+        self.add_colloids_to_image(colloids)
+        self.positionCameraAboveCenter()
         image = self.renderer.render_to_image()
         self.logger.debug("Screen captured")
         self.renderer.scene.clear_geometry()
         self.logger.info("Top-down image computed")
-        return self.rgb2gray(np.asarray(image))
-
-
-
+        image=self.rgb2gray(np.asarray(image))
+        return np.asarray(image)
 
