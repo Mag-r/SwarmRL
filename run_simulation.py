fixed gradient computation

run_simulation.py
@@ -40,7 +40,7 @@ class ActoCriticNet(nn.Module):
     When dense at beginning, probably flatten is required
     """
 
-    features: int = 32  # Number of LSTM features
+
 
     def setup(self):
         # Define a scanned LSTM cell
@@ -60,7 +60,7 @@ class ActoCriticNet(nn.Module):
         # x = nn.avg_pool(x, window_shape=(3, 3), strides=(3, 3))
 
         # Apply ConvLSTM
-        lstm = self.ScanLSTM(features=32, kernel_size=(3, 3))
+        lstm = self.ScanLSTM(features= 12, kernel_size=(3, 3))
 
         # Initialize carry if it's not provided
         if carry is None:
@@ -72,14 +72,14 @@ class ActoCriticNet(nn.Module):
 
         # Apply relu activation to the LSTM output (memory)
         x = nn.relu(memory)
-        x = nn.Conv(features=32, kernel_size=(3, 3), strides=(3, 3))(x)
+        x = nn.Conv(features=12, kernel_size=(3, 3), strides=(3, 3))(x)
         x = nn.relu(x)
 
-        x = nn.Conv(features=32, kernel_size=(3, 3), strides=(3, 3))(x)
+        x = nn.Conv(features=12, kernel_size=(3, 3), strides=(3, 3))(x)
         x = nn.relu(x)
         x = nn.avg_pool(x,window_shape=(3,3),strides=(3,3))
 
-        x = nn.Conv(features=32, kernel_size=(3, 3), strides=(3, 3))(x)
+        x = nn.Conv(features=12, kernel_size=(3, 3), strides=(3, 3))(x)
         x = nn.relu(x)
 
         
@@ -91,17 +91,17 @@ class ActoCriticNet(nn.Module):
         # x = x.reshape((sequence_length, -1))
         # x = nn.relu(x)
 
-        x = nn.Dense(features=64)(x)
+        x = nn.Dense(features=12)(x)
         x = nn.relu(x)      
         x = x.flatten()
 
-        y = nn.Dense(features=64)(x)
-        x = nn.Dense(features=64)(x)
+        y = nn.Dense(features=12)(x)
+        x = nn.Dense(features=12)(x)
         x = nn.relu(x)
         y = nn.relu(y)
 
-        y = nn.Dense(features=32)(y)
-        x = nn.Dense(features=128)(x)
+        y = nn.Dense(features=12)(y)
+        x = nn.Dense(features=12)(x)
         x = nn.relu(x)
         y = nn.relu(y)
 

swarmrl/losses/global_policy_gradient_loss.py
@@ -92,16 +92,15 @@ class GlobalPolicyGradientLoss(Loss):
 
         # (n_timesteps, n_particles)
         advantage = returns - predicted_values
-        advantage = (advantage - jnp.mean(advantage)) / (jnp.std(advantage) + 1e-8)
+        # advantage = (advantage - jnp.mean(advantage)) / (jnp.std(advantage) + 1e-8)
 
-        # logger.info(f"{advantage=}")
-
-        actor_loss = - 1 * ((log_probs * advantage).sum(axis=0)).sum()
-        logger.debug(f"{actor_loss=}")
-
+        logger.debug(f"{advantage=}")
         # Sum over time steps and average over agents.
-        critic_loss =  1 * optax.huber_loss(predicted_values, returns).sum(axis=0).sum()
-        # logger.info(f"{critic_loss=}, {actor_loss=}")
+        critic_loss =  1 * optax.huber_loss(predicted_values, returns).sum()
+        advantage = jax.lax.stop_gradient(advantage)
+        actor_loss = - 1 * (log_probs * advantage).sum()
+        
+        logger.debug(f"{critic_loss=}, {actor_loss=}")
         return actor_loss + critic_loss
 
     def compute_loss(self, network: Network, episode_data):